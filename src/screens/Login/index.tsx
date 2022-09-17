d888659f4371552707a7dfa6faee92d969370264
William James <52788612+william-james-pj@users.noreply.github.com>
2022-09-17 10:03:54 -0300

    Create Welcome and Login screen

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { KeyboardAvoidingView, Platform, ScrollView } from "react-native";
+
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { ButtonLarge } from "../../components/ButtonLarge";
+import { TextInput } from "../../components/TextInput";
+
+import { emailValidator } from "../../utils/emailValidator";
 
 import * as S from "./styles";
 
 export function Login() {
-  return <S.WrapperBackgroud></S.WrapperBackgroud>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <S.WrapperBackgroud>
+      <SafeAreaView style={{ flex: 1 }}>
+        <KeyboardAvoidingView
+          style={{ flex: 1 }}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+          <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+            <S.Wrapper>
+              <S.Header>
+                <S.Title>Bem vindo de volta</S.Title>
+                <S.SubTitle>
+                  Você pode continuar de onde{"\n"}parou fazendo login.
+                </S.SubTitle>
+                <S.Form>
+                  <TextInput
+                    placeholder={"E-mail"}
+                    onChangeText={setEmail}
+                    value={email}
+                    validator={emailValidator}
+                    errorText={"Por favor insira um endereço de e-mail válido"}
+                    keyboardType={"email-address"}
+                  />
+                  <TextInput
+                    placeholder={"Senha"}
+                    onChangeText={setPassword}
+                    value={password}
+                    secureTextEntry={true}
+                  />
+                  <S.TextFogot>Esqueceu a senha?</S.TextFogot>
+                </S.Form>
+                <ButtonLarge onPress={() => {}} text={"Entrar"} />
+              </S.Header>
+            </S.Wrapper>
+          </ScrollView>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    </S.WrapperBackgroud>
+  );
 }
--- src/screens/Welcome/styles.ts
+++ src/screens/Welcome/styles.ts
@@ -1,7 +1,33 @@
 import styled from "styled-components/native";
 import { fonts } from "../../styles/fonts";
 
-export const WrapperBackgroud = styled.View`
+export const Wrapper = styled.View`
   background: ${(props) => props.theme.colors.background};
   flex: 1;
+  display: flex;
+  padding: 32px 16px 32px 16px;
+`;
+
+export const Header = styled.View`
+  height: 80%;
+  width: 100%;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+export const TextContainer = styled.View`
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  color: ${(props) => props.theme.colors.primary};
+  font-size: ${fonts.size.xxl};
+  font-family: ${fonts.type.text700};
+  margin: 0 0 16px 0;
+`;
+
+export const SubTitle = styled.Text`
+  color: ${(props) => props.theme.colors.disabled};
+  font-size: ${fonts.size.normal};
+  font-family: ${fonts.type.text400};
 `;
